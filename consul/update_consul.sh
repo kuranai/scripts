b7a836b555d4addd21ddaa239f9dcac406c57730
Alexander Klatte <ak@doozer.de>
2019-04-08 21:21:13 +0200

    first commit

---
+++ consul/update_consul.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+consul_version="1.4.4"
+
+mkdir -p /tmp/consul_update
+cd /tmp/consul_update
+
+systemctl stop consul
+
+wget https://releases.hashicorp.com/consul/${consul_version}/consul_${consul_version}_linux_amd64.zip
+unzip consul_${consul_version}_linux_amd64.zip
+mv consul /usr/local/bin/
+
+cd -
+rm -r /tmp/consul_update
+
+systemctl daemon-reload
+systemctl start consul
+systemctl status consul
+consul members
---
+++ consul/update_consul_docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+env=${1}
+consul_version="1.4.4"
+
+# stopping consul-related containers
+docker ps | grep "consul" | cut -d. -f1 |  awk '{print $1}' | xargs docker stop
+
+# removing consul-related containers
+docker container ls | grep "consul" | cut -d. -f1 | awk '{print $1}' | xargs docker container rm
+
+# running reverse consul agent
+docker run -d --net=host --restart=unless-stopped -h `curl -s http://169.254.169.254/latest/meta-data/instance-id` --name consul-agent \
+        -v /opt/consul:/consul/data -v /var/run/docker.sock:/var/run/docker.sock \
+        consul:${consul_version} agent -data-dir=/consul/data -client=0.0.0.0 -join consul-1.${env}.doozer.internal -advertise `curl -s http://169.254.169.254/latest/meta-data/local-ipv4`
+
+# running service registrator with autoclean (-cleanup) flag to remove dangling services
+docker run -d --net=host --restart=unless-stopped -h INSTANCE_ID=`curl -s http://169.254.169.254/latest/meta-data/instance-id` --name consul-registrator \
+        -v /var/run/docker.sock:/tmp/docker.sock \
+        gliderlabs/registrator -cleanup -ip `curl -s http://169.254.169.254/latest/meta-data/local-ipv4` consul://localhost:8500
