eb2ea345f3c013a8b1c1eeb96838da5712c2e394
Alexander Klatte <ak@doozer.de>
2019-04-09 09:32:51 +0200

    finished README.md

--- consul/README.md
+++ consul/README.md
@@ -1,3 +1,11 @@
 # Consul Update Scripts
 
-Scripts are selfexplained
+Steps are commented on the scripts
+
+### update consul as a service on ubuntu
+* run: `curl https://raw.githubusercontent.com/ktxcx/scripts/master/consul/update_consul.sh | sudo bash`
+
+### update consul running in a docker environment (aws-linux)
+* run: 
+  `export env=<environment>`<br>
+  `curl https://raw.githubusercontent.com/ktxcx/scripts/master/consul/update_consul_docker.sh | bash`
--- consul/update_consul.sh
+++ consul/update_consul.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 consul_version="1.4.4"
 
+# create a tempoary folder for the consul download
 mkdir -p /tmp/consul_update
 cd /tmp/consul_update
 
+# stop running consul
 systemctl stop consul
 
+# wait 3 secs
+sleep 3
+
+# download and unzip consul binary
 wget https://releases.hashicorp.com/consul/${consul_version}/consul_${consul_version}_linux_amd64.zip
 unzip consul_${consul_version}_linux_amd64.zip
 mv consul /usr/local/bin/
-
 cd -
+
+# cleanup
 rm -r /tmp/consul_update
 
+
 systemctl daemon-reload
+
 systemctl start consul
 systemctl status consul
 
+# print the consul memberlist
+sleep 1
 /usr/local/bin/consul members
