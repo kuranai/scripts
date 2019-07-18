02a34ca70a44d7bfc6fa3ce210924cfd2f73bcd7
Alexander Klatte <ak@doozer.de>
2019-07-18 16:24:57 +0200

    quickfix: removed duplicated curl install command

--- docker/install_docker.sh
+++ docker/install_docker.sh
@@ -5,7 +5,7 @@ linux_os=$(cat /etc/issue|cut -d" " -f1|head -1|awk '{print tolower($0)}')
 docker_compose_ver=1.24.0
 
 apt-get remove docker docker-engine docker.io -y
-apt-get install curl gnupg apt-transport-https ca-certificates curl software-properties-common -y
+apt-get install gnupg apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/${linux_os}/gpg | apt-key add -
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/${linux_os} \
