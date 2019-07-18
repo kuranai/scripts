9b53fdd84568a6c85edc32b033be2b1c68eed3b2
Alexander Klatte <ak@doozer.de>
2019-07-18 16:20:12 +0200

    remove not needed apt commands and add os swtich to detect of debian or ubuntu

--- docker/install_docker.sh
+++ docker/install_docker.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
+
+linux_os=$(cat /etc/issue|cut -d" " -f1|head -1|awk '{print tolower($0)}')
+
 docker_compose_ver=1.24.0
 
 apt-get remove docker docker-engine docker.io -y
-apt-get install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+apt-get install curl gnupg apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/${linux_os}/gpg | apt-key add -
 add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   "deb [arch=amd64] https://download.docker.com/linux/${linux_os} \
    $(lsb_release -cs) \
    stable"
 
-apt-get update && apt upgrade -y
-apt-get install docker-ce -y
+apt update 
+apt install docker-ce -y
 
-groupadd docker && usermod -aG docker ubuntu
+case $linux_os in
+   ubuntu)
+      groupadd docker && usermod -aG docker ubuntu
+      ;;
+   *)
+      groupadd docker
+      ;;
+esac
 
 systemctl enable docker
 systemctl start docker
-systemctl status docker
-
-apt-get autoremove -y
 
 curl -L https://github.com/docker/compose/releases/download/${docker_compose_ver}/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
